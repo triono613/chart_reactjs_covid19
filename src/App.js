b72b42791b3d732be423b5e14d9a97fefa8aa57f
triono <triono.triono1@gmail.com>
2022-02-20 19:21:36 +0700

    fix new repo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import BarChart from './components/BarChart';
-import PieChart from './components/PieChart';
+
 import { Navbar } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
  
 
 const App = () => {
-  // return 
-  // <div>
-  //   <BarChart />
-  //   <PieChart />
-  // </div>
 
   return (
 <div className="App">
